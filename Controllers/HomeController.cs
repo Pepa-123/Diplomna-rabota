8291774f4ecbfb5964cd015844ad9a69e97e2a83
Pepa-123 <pb43385495@edu1.pgzaimov.com>
2023-05-22 10:14:00 +0300

    change footer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
